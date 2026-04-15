da1edbad8ac245a18c5b196b768334ccb5fe5afa
anelelubai0808-design <anelelubai0808@gmail.com>
2026-04-15 08:30:36 +0500

    Add files via upload

---
+++ JS/form.js
@@ -0,0 +1,59 @@
+// Простая библиотека книг - 1 курс
+const formBook = document.querySelector("#books-form");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const year = document.querySelector("#year");
+const genre = document.querySelector("#genre");
+
+const rawData = localStorage.getItem("books");
+let books = rawData ? JSON.parse(rawData) : [];
+
+// Загрузка книг из памяти браузера
+function loadBooks() {
+  let saved = localStorage.getItem("books");
+  if (saved) {
+    books = JSON.parse(saved);
+  }
+  showBooks();
+}
+
+// Сохранение книг в память браузера
+function saveBooks() {
+  localStorage.setItem("books", JSON.stringify(books));
+}
+
+// Добавление книги
+formBook.addEventListener("submit", (e) => {
+  e.preventDefault(); // Отмена перезагрузки страницы
+  const newItem = {
+    title: title.value,
+    author: author.value,
+    year: year.value,
+    genre: genre.value,
+  };
+  books.push(newItem); // Добавляем в массив
+  saveBooks(); // Сохраняем в localStorage
+  author.value = ""; // Очищаем поле ввода
+  title.value = "";
+  year.value = "";
+  genre.value = "";
+});
+
+// if (formBook) {
+//   formBook.onsubmit = function (e) {
+//     e.preventDefault();
+
+//     let title = document.getElementById("title").value;
+//     let author = document.getElementById("author").value;
+
+//     if (title && author) {
+//       books.push({ title: title, author: author });
+//       console.log(books);
+//       saveBooks();
+//       alert("Книга добавлена!");
+//       form.reset();
+//     } else {
+//       alert("Заполните название и автора!");
+//     }
+//   };
+// }
---
+++ JS/index.js
@@ -0,0 +1,45 @@
+// Простая библиотека книг - 1 курс
+const rawData = localStorage.getItem("books");
+let books = rawData ? JSON.parse(rawData) : [];
+
+// Загрузка книг из памяти браузера
+function loadBooks() {
+  let saved = localStorage.getItem("books");
+  if (saved) {
+    books = JSON.parse(saved);
+  }
+  showBooks();
+}
+
+// Показ всех книг
+function showBooks() {
+  let list = document.getElementById("book-list");
+  let total = document.getElementById("count");
+  total.textContent = books.length;
+  console.log(list);
+  list.innerHTML = "";
+  //   total.textContent = books.length;
+  console.log(books);
+
+  if (books.length == 0) {
+    list.innerHTML =
+      "<p style='text-align:center; color:gray; padding:30px;'>Пока нет книг</p>";
+    return;
+  }
+
+  for (let i = 0; i < books.length; i++) {
+    let div = document.createElement("div");
+    div.className = "book-card";
+    div.innerHTML = `
+            <h3>${books[i].title}</h3>
+            <p>Автор: ${books[i].author}</p>
+            <p>год издания: ${books[i].year}</p>
+            <p>Жанр: ${books[i].genre}</p>
+            <button onclick="deleteBook(${i})">Удалить</button>
+        `;
+    list.appendChild(div);
+  }
+}
+
+// Запуск программы
+loadBooks();
